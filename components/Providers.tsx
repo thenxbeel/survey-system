1b3cd269ec34a82bc24022a32af64def06703c47
Nabeel Habees <thenxbeel@gmail.com>
2026-07-09 01:21:05 +0400

    Fix Providers import build error

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import type { ReactNode } from 'react'
-import { SettingsProvider } from '@/components/settings/SettingsProvider'
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <SettingsProvider>{children}</SettingsProvider>
+  return <>{children}</>
 }
